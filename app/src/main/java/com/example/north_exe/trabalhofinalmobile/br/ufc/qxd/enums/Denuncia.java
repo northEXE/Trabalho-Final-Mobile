5057c35390f25fc8988376351e20aad6bba30887
Wendel Ferreira de Mesquita <north-exe@north.exe>
2017-11-23 13:00:25 -0300

    Criação da classe de Denuncia e da Enum do tipo de categoria da denuncia

---
+++ app/src/main/java/com/example/north_exe/trabalhofinalmobile/br/ufc/qxd/enums/Denuncia.java
@@ -0,0 +1,14 @@
+package com.example.north_exe.trabalhofinalmobile.br.ufc.qxd.enums;
+
+/**
+ * Created by north-exe on 23/11/17.
+ */
+
+public enum Denuncia {
+    VIAS_PUBLICAS(1), EQUIPAMENTOS_COMUNITARIOS(2), LIMPEZA_URBANA(3);
+
+    private final int valor;
+    Denuncia(int valor) {
+        this.valor = valor;
+    }
+}
---
+++ app/src/main/java/com/example/north_exe/trabalhofinalmobile/br/ufc/qxd/model/Denuncia.java
@@ -0,0 +1,18 @@
+package com.example.north_exe.trabalhofinalmobile.br.ufc.qxd.model;
+
+import java.util.Date;
+
+/**
+ * Created by north-exe on 23/11/17.
+ */
+
+public class Denuncia {
+    private Integer id;
+    private String descricao;
+    private Date data;
+    private Double longitude;
+    private Double latitude;
+    private String uriMidia;
+    private String usuario;
+    private Enum categoria;
+}
